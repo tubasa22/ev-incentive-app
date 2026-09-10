4d2270c8496dedf3a27c826abe72d3898c15bb3f
tubasa22 <tubasa22@gmail.com>
2026-09-10 12:20:01 -0700

    클린EV 랜딩과 소개 페이지 디자인 개선

--- site-config.js
+++ site-config.js
@@ -1,2 +1,8 @@
 /* 모든 공개 페이지의 공통 연락처 설정입니다. */
-window.SITE_CONFIG = { contactEmail: 'jdlee.electric@gmail.com' };
+window.SITE_CONFIG = {
+  contactEmail: 'jdlee.electric@gmail.com',
+  images: {
+    hero: 'https://source.unsplash.com/800x600/?electric-car,home-charging',
+    about: 'https://source.unsplash.com/800x600/?family,korean'
+  }
+};
